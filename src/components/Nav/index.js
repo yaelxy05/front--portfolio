285f8b00d9a441d6d6a319de5795af2df434484b
yaelx <yaelx@localhost>
2021-08-26 23:11:08 +0200

    add css nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,25 +8,27 @@ import './nav.scss';
 // == Composant
 const Nav = () => (
   <div className="nav">
-    <div class="nav_logo--box">
-      <img className="nav_logo" src={logoNav} alt="logo" />
-    </div>
+    <div class="nav_banner--top">
+      <div class="nav_logo--box">
+        <img className="nav_logo" src={logoNav} alt="logo" />
+      </div>
 
-    <div class="nav_bar">
-      <ul>
-        <li>
-          <i class="fas fa-home"></i>
-        </li>
-        <li>Compétences</li>
-        <li>Mes créations</li>
-        <li>A propos</li>
-        <li>Contact</li>
-      </ul>
+      <div class="nav_bar">
+        <ul>
+          <li>
+            <i class="fas fa-home"></i>
+          </li>
+          <li>Compétences</li>
+          <li>Mes créations</li>
+          <li>A propos</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
     <div class="nav_banner">
-      <p class="nav_banner--p">
-        Bonjour, Je suis Yaël HUE Développeur web Fullstack
-      </p>
+      <p>Bonjour, </p>
+      <p>Je suis Yaël HUE</p>
+      <p>Développeur web Fullstack</p>
     </div>
   </div>
 );
