0b3da78df64dfea18f1869f3cf5129808d2c01ff
yaelx <yaelx@localhost>
2021-08-27 12:14:05 +0200

    create css footer and reajust shadow nav-bar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,24 @@ import './footer.scss';
 // == Composant
 const Footer = () => (
   <div className="footer">
-    
+    <div className="footer_info">
+      <div className="footer_info--box footer_local">
+        <i className="fas fa-map-marker-alt"></i>
+        <p>
+          236 Route de Rabou <br />
+          05400 La Roche des Arnauds <br />
+          France
+        </p>
+      </div>
+      <div className="footer_info--box footer_mail">
+        <i className="far fa-envelope"></i>
+        <p>yaelhue@gmail.com</p>
+      </div>
+      <div className="footer_info--box footer_phone">
+        <i className="fas fa-mobile-alt"></i>
+        <p>07.81.28.51.47</p>
+      </div>
+    </div>
   </div>
 );
 
