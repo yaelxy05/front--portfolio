461ab0841f9c35120002a5be1a39fd451061ce64
yaelx <yaelx@localhost>
2021-09-14 23:06:36 +0200

    add card post creations

--- src/components/Creations/index.js
+++ src/components/Creations/index.js
@@ -2,12 +2,39 @@
 import React, { useState } from 'react';
 // == Import
 import './creations.scss';
-
+// Import img
+import screenDpm from '../../assets/images/screenshot_creations/desktop_screenshot-dpm.png';
+import screenPortfolio from '../../assets/images/screenshot_creations/desktop_screenshot-yaelcreations.png';
 // == Composant
 const Creations = () => {
   return (
     <div className="creations">
       <h1 className="creations_h1">Mes créations</h1>
+
+      <div className="creations_box">
+        <div className="creations_item">
+          <img
+            className="img_creations"
+            src={screenDpm}
+            alt="capture d'écran site dressing des petites mains"
+          />
+          <div className="creations_title--box">
+            <h2>Dressing des petites mains</h2>
+            <i className="fab fa-github"></i>
+          </div>
+        </div>
+        <div className="creations_item dpm">
+          <img
+            className="img_creations"
+            src={screenPortfolio}
+            alt="capture d'écran site dressing des petites mains"
+          />
+          <div className="creations_title--box">
+            <h2>Mon portfolio</h2>
+            <i className="fab fa-github"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
