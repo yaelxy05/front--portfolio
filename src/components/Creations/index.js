fcfb9c44e8e63b2db7b56b604b0bc8331ebc3f87
yaelx <yaelx@localhost>
2021-09-30 21:36:19 +0200

    add link to creations page

--- src/components/Creations/index.js
+++ src/components/Creations/index.js
@@ -14,20 +14,28 @@ const Creations = () => {
       <div className="creations_box">
         <div className="creations_item">
           <div className="img_creations img_dpm">
-            <p>Découvrez le site</p>
+            <a href="#">
+              <p>Découvrez le site</p>
+            </a>
           </div>
           <div className="creations_title--box">
             <h2>Dressing des petites mains</h2>
-            <i className="fab fa-github"></i>
+            <a href="https://github.com/yaelxy05/dpm">
+              <i className="fab fa-github"></i>
+            </a>
           </div>
         </div>
         <div className="creations_item dpm">
           <div className="img_creations img_portfolio">
-            <p>Découvrez le site</p>
+            <a href="http://yaelhue-creation.com/">
+              <p>Découvrez le site</p>
+            </a>
           </div>
           <div className="creations_title--box">
             <h2>Mon portfolio</h2>
-            <i className="fab fa-github"></i>
+            <a href="https://github.com/yaelxy05/front--portfolio">
+              <i className="fab fa-github"></i>
+            </a>
           </div>
         </div>
       </div>
